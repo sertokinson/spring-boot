0a19e76edccc01d5d2cc09283cf3ef4147a85b9f
sertokinson <sertokinson@gmail.com>
2018-11-19 16:49:52 +0300

    Работоспособный SignInController

--- src/main/java/ru/sertok/spring/boot/controllers/SignInController.java
+++ src/main/java/ru/sertok/spring/boot/controllers/SignInController.java
@@ -1,26 +1,20 @@
 package ru.sertok.spring.boot.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-import ru.sertok.spring.boot.repositories.UsersRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 @Controller
-@RequestMapping(path = "/signIn")
 public class SignInController {
 
-    @Autowired
-    private UsersRepository usersRepository;
-
-    @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView get() {
-        return new ModelAndView("signIn");
+    @GetMapping("/signIn")
+    public String get() {
+        return "signIn";
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping("/signIn")
     public String post() {
             return "redirect:/users";
     }
